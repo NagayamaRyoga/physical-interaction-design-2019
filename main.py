cb5a6b8cdc87866678abb7ce5aa845d5dc10b32d
NagayamaRyoga <nagayama15@sec.is.kit.ac.jp>
2019-07-30 11:22:28 +0900

    Extends the number of LEDs

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import weather
 import serial_led
 
 LED_PIN = 18
-NUM_LED = 9
+NUM_LED = 19
 API_KEY_NAME = "OPEN_WEATHER_MAP_API_KEY"
 
 COLOR_SUNNY = [0xff, 0x00, 0x00]
@@ -24,6 +24,35 @@ class ApiKeyNotExistException(Exception):
     def __init__(self, message):
         self.message = message
 
+def lerp(a, b, t):
+    return (b - a) * t + a
+
+def lerpColor(a, b, t):
+    return [int(lerp(x, y, t)) for x, y in zip(a, b)]
+
+def weatherColor(cloudiness, rain_3h):
+    if rain_3h == None:
+        return lerpColor(COLOR_SUNNY, COLOR_CLOUDY, cloudiness / 100)
+    else:
+        return lerpColor(COLOR_CLOUDY, COLOR_RAINY, min(rain_3h * 0.3, 1))
+
+def buildLEDColor(forecasts, i):
+    if i % 3 == 0:
+        f = forecasts[i // 3]
+        return weatherColor(f.cloudiness, f.rain_3h)
+
+    a = weatherColor(forecasts[i // 3].cloudiness, forecasts[i // 3].rain_3h)
+    b = weatherColor(forecasts[i // 3 + 1].cloudiness, forecasts[i // 3 + 1].rain_3h)
+    print(i % 3 / 3)
+    return lerpColor(a, b, (i % 3) / 3)
+
+def buildLEDColors(forecasts):
+    # From 06:00 ~ 24:00 (length of 7)
+    forecasts = forecasts[1:8]
+
+    # Returns the LED colors (Froms 06:00 ~ 24:00, 1h each, length of 19)
+    return [buildLEDColor(forecasts, i) for i in range(19)]
+
 def main():
     print("main")
 
@@ -39,21 +68,8 @@ def main():
     # Setup the serial LED
     led = serial_led.SerialLED("./serial-led-pi/serialled.so", LED_PIN, NUM_LED)
 
-    # Build the colors
-    def lerp(a, b, t):
-        return (b - a) * t + a
-
-    def lerpColor(a, b, t):
-        return [int(lerp(x, y, t)) for x, y in zip(a, b)]
-
-    def weatherColor(cloudiness, rain_1h):
-        if rain_1h == None:
-            return lerpColor(COLOR_SUNNY, COLOR_CLOUDY, cloudiness / 100)
-        else:
-            return lerpColor(COLOR_CLOUDY, COLOR_RAINY, max(rain_1h * 1, 1))
-
-    led.send([weatherColor(w.cloudiness, w.rain_3h / 3 if w.rain_3h else None)
-        for w in response.forecasts[:NUM_LED]])
+    # Light the LED up
+    led.send(buildLEDColors(response.forecasts))
 
     time.sleep(10)
 
